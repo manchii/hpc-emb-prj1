f1d100acb3a3d5c271f42c469b7e30b0a9c1c2f9
manchii <kaleb.23415@gmail.com>
2019-06-20 15:48:46 -0600

    Ingreso de macro para saturar

--- assignment-2/Code/src/rgb2yuv.hpp
+++ assignment-2/Code/src/rgb2yuv.hpp
@@ -7,12 +7,10 @@ void RGB2YUV(const ContRGB RGB, ContYUV YUV) noexcept {
     const int r=RGB[indexRGB];
     const int g=RGB[indexRGB+1];
     const int b=RGB[indexRGB+2];
-    y = ((66*r+129*g+25*b+128)>>8)+16;
-    u = ((-38*r-74*g+112*b+128)>>8)+128;
-    v = ((112*r-94*g-18*b+128)>>8)+128;
-    y = y<255?y:255; y = y<0?0:y;
-    u = u<255?u:255; u = u<0?0:u;
-    v = v<255?v:255; v = v<0?0:v;
+    #define SATURATE(X) (X<0)?0 : (X<255?X:255);
+    y = SATURATE(((66*r+129*g+25*b+128)>>8)+16);
+    u = SATURATE(((-38*r-74*g+112*b+128)>>8)+128);
+    v = SATURATE(((112*r-94*g-18*b+128)>>8)+128);
     YUV.y[indexYUV]=static_cast<uint8_t>(y);
     YUV.u[indexYUV]=static_cast<uint8_t>(u);
     YUV.v[indexYUV]=static_cast<uint8_t>(v);
