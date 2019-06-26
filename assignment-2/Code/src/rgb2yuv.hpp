e83406be31aebeeb69bdc39227f9d64fedbe24d0
moradoug <dmc220190@gmail.com>
2019-06-25 20:16:58 -0700

    Documentation

--- assignment-2/Code/src/rgb2yuv.hpp
+++ assignment-2/Code/src/rgb2yuv.hpp
@@ -1,4 +1,11 @@
+
 #ifndef __ARM_NEON__
+
+/*---------------------------Generic_Version----------------------------------/
+This code serves as a placeholder for non-NEON compatible processor (ARMCPU)
+The precision used is 8-bits signed fixed-point.
+This implementation assumes RGB888 packed format and converts it to YUV(4:4:4)p 
+/----------------------------------------------------------------------------*/
 template<typename ContRGB,typename ContYUV>
 void RGB2YUV(const ContRGB RGB, ContYUV YUV) noexcept {
   uint indexRGB;
@@ -19,28 +26,44 @@ void RGB2YUV(const ContRGB RGB, ContYUV YUV) noexcept {
 }
 
 #else
-template <typename T>
-void printVec(T vect,int size=8){
-  std::cout << "Vec: ";
-  for(int i=0; i<size; i++){
-    std::cout << +vect[i] <<"__";
-  }
-  std::cout << "\n";
-}
+
+/*-----------------------------NEON_Intrinsics_Version-------------------------/
+In this section the code is being represented with intrisics for NEON
+This helps to perform 8 opertions per instruction which helps to improve
+the performance of the program.
+Note that in this case the 7-bits signed fixed-point of pecision is being
+used instead of the 8-bits that are being used in the previous version (Generic Version).
+Thus, the following coefficients are divided by two from the previous
+implementation. 
+XXX For more information check the attached spreadsheet in the
+    repository (Description_RGB2YUVNEON) XXX
+/-----------------------------------------------------------------------------*/ 
 
 #include <arm_neon.h>
 template<typename ContRGB,typename ContYUV>
+
 void RGB2YUV(const ContRGB RGB, ContYUV YUV) noexcept {
   uint indexRGB; uint indexYUV;
   for(indexRGB=0,indexYUV=0; indexRGB<RGB.size(); indexRGB+=24,indexYUV+=8){
+    //vld3_u8
+    //RGBRGBRGBRGBRGBRGBRGBRGB
+    //to
+    //RRRRRRRR
+    //GGGGGGGG
+    //BBBBBBBB
     const uint8x8x3_t rgb = vld3_u8(&RGB[indexRGB]);
+
+    //Assign Var
     const uint8x8_t &r8 = rgb.val[0];
     const uint8x8_t &g8 = rgb.val[1];
     const uint8x8_t &b8 = rgb.val[2];
+
+    //Change from 8x8 to 16x8
     const auto R16 = vreinterpretq_s16_u16(vmovl_u8(r8));
     const auto G16 = vreinterpretq_s16_u16(vmovl_u8(g8));
     const auto B16 = vreinterpretq_s16_u16(vmovl_u8(b8));
-    
+
+    //This macro calculate the equation Y=((c1*R+c2*G+c3*B)>>7)+c4
     #define VALUE(R,G,B,c1,c2,c3,c4,S){\
     	auto Y16 = int16x8_t{64,64,64,64,64,64,64,64};\
 	auto c  = int16x8_t{c1 ,c1 ,c1 ,c1 ,c1 ,c1 ,c1 ,c1 };\
@@ -55,6 +78,9 @@ void RGB2YUV(const ContRGB RGB, ContYUV YUV) noexcept {
 	auto y8 = vreinterpret_u8_s8(vmovn_s16(Y16));\
 	vst1_u8(&S,y8);}
 
+    //Y=((33*R+65*G+13*B)>>7)+16
+    //U=((-19*R+-37*G+56*B)>>7)+128
+    //V=((56*R+-47*G+-9*B)>>7)+128
     VALUE(R16,G16,B16,33,65,13,16,YUV.y[indexYUV]);
     VALUE(R16,G16,B16,-19,-37,56,128,YUV.u[indexYUV]);
     VALUE(R16,G16,B16,56,-47,-9,128,YUV.v[indexYUV]);
